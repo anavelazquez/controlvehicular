d581ccc6d9cef14bcd59b325228ad3db3c4a8ae7
Daniel Luna S <seluda@users.noreply.github.com>
2020-11-10 11:52:10 -0600

    index.php
    primera actualización

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="esp">
+<head>
+	<meta charset="UTF-8">
+		<title>Login SISCV</title>
+		<link rel="stylesheet" hrfe="css/estilos.css">
+
+<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximun-scale=1.0, minimun-scale=1.0">
+	</head>
+<body>
+	<div class="contaner-all">
+		<div class="ctn-form">
+			<img src=" alt=" class="logo">
+			<h1 class="title">Iniciar Sesión</h1>
+
+		<form action="inicio">
+			<label for="">Email</label>
+			<input type="text"></input>
+			<label for="">Contraseña</label>
+			<input type="password"></input>
+		</form>
+		</div>
+			<div class="ctn-text">
+				<div class="capa"></div>
+				<h1 class="title-description">Sistema de Control Vehicular de la SIOP</h1>
+			</div>
+	</div>
+</body>
+</head>
