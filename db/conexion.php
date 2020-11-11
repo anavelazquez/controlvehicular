d0319d5b1b08a1ecee1832c783163a238000d873
Ana Velazquez <anita.velazquez55@gmail.com>
2020-11-11 09:20:19 -0600

    insert usuario

--- db/conexion.php
+++ db/conexion.php
@@ -1,14 +1,14 @@
 <?php
-	$conexion = mysqli_connect("localhost","root","","vehicular");
+	$conexion = mysqli_connect("localhost","root","","control_vehicular");
 
 if(!$conexion)
 	{
 		echo 'error No:'.mysqli_connect_errno();
-		echo 'error Description'.mysqli_connect_error();	
+		echo 'error Description'.mysqli_connect_error();
 	}
 else
 	{
-	echo "se conecto la base de datos";
+	//echo "se conecto la base de datos";
 	}
 
 ?>
