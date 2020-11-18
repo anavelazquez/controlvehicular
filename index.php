e0a1f524d397993ca839734b0872401c4bbda0cb
daniel <dan_luna@hotmail.es>
2020-11-17 23:34:33 -0600

    agrege bootstrap

--- index.php
+++ index.php
@@ -6,23 +6,20 @@
 <head>
 	<meta charset="UTF-8">
 		<title>Login SISCV</title>
-		<link rel="stylesheet" hrfe="css/estilos.css">
+		<link rel="stylesheet" href="css/bootstrap.min.css" />
 
 <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximun-scale=1.0, minimun-scale=1.0">
 	</head>
 <body>
 	<div class="contaner-all">
-		<div class="ctn-form">
-			<img src=" alt=" class="logo">
-			
+		<div class="mi-web w-75 m-auto p-5 border">
 			<h1 class="title">Iniciar Sesión</h1>
-
 		<form name="registro" action="funciones/ingresas.php" method="POST">
 			<label for="">Usuario</label>
-			<input name="usuario" type="text"></input>
-			<label for="">Contraseña</label>
+			<br><input name="usuario" type="text"></input></br>
+			<br><label for="">Contraseña</label></br>
 			<input name="password" type="password"></input>
-			<button type="submit">Entrar</button>
+			<button class="btn btn-success">Entrar</button>
 		</form>
 		</div>
 			<div class="ctn-text">
@@ -30,5 +27,9 @@
 				<h1 class="title-description">Sistema de Control Vehicular de la SIOP</h1>
 			</div>
 	</div>
+
+	<script type="text/javascript" src="jquery/jquery-3.5.1.min.js"></script>
+<script type="text/javascript" src="js/bootstrap.min.js00"></script>
 </body>
+
 </head>
