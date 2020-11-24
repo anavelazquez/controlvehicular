3afc006999b77a1e9635b07e4bd1af9a94fb6ec0
Daniel Luna S <seluda@users.noreply.github.com>
2020-11-24 14:24:13 -0600

    footer

---
+++ footer.php
@@ -0,0 +1,10 @@
+<!doctype html>
+<html lang="esp">
+<footer class="text-muted">
+  <div class="container">
+    <p class="float-right">
+    </p>
+    <p>Sistema de Control Vehicular &copy; Departamento de Tecnologías de la Información SIOP</p>
+  </div>
+</footer>
+</html>
